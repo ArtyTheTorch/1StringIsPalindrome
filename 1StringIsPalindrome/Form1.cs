ef57b7c114458adf8abb10d4ab13fc80ad96b86d
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 11:11:14 -0700

    Check String To See If It's a Palindrome
    Implemented the palindrome class as well as made a UI for this project

---
+++ 1StringIsPalindrome/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StringIsPalindrome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Using Recursion
+            IsPalindrome p = new IsPalindrome();
+            label1.Text = p.isPalindrome(textBox1.Text).ToString();
+
+
+        }
+    }
+}
